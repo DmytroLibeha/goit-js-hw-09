3ac60ac21c6dcba959f752c26bf1019d1176e70d
DmytroLibeha <granplus84@gmail.com>
2024-11-20 17:36:51 +0100

    add css

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,50 @@
-console.log('Form');
+const formData = {
+  email: '',
+  message: '',
+};
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector("input[name='email']");
+const messageInput = form.querySelector("textarea[name='message']");
+
+document.addEventListener('DOMContentLoaded', () => {
+  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (savedData) {
+    formData.email = savedData.email || '';
+    formData.message = savedData.message || '';
+    emailInput.value = formData.email;
+    messageInput.value = formData.message;
+  }
+});
+
+form.addEventListener('input', event => {
+  const { name, value } = event.target;
+
+  if (name === 'email') {
+    formData.email = value.trim();
+  } else if (name === 'message') {
+    formData.message = value.trim();
+  }
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  if (formData.email === '' || formData.message === '') {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log('Form Data Submitted:', formData);
+
+  localStorage.removeItem(STORAGE_KEY);
+  formData.email = '';
+  formData.message = '';
+  emailInput.value = '';
+  messageInput.value = '';
+});
